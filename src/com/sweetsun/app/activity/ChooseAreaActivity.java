ce4f01680b3163e66a643be1516b5c1ccabb46a6
harrytien <harrytien@163.com>
2015-01-20 12:52:07 +0800

    base

---
+++ src/com/sweetsun/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,218 @@
+package com.sweetsun.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sweetsun.app.SweetSunDB;
+import com.sweetsun.app.model.*;
+import com.sweetsun.app.util.HttpCallbaceListener;
+import com.sweetsun.app.util.HttpUtil;
+import com.sweetsun.app.util.Utility;
+
+import android.R;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ChooseAreaActivity extends Activity {
+	public static final int LEVEL_PROVINCE = 0;
+	public static final int LEVEL_CITY = 1;
+	public static final int LEVEL_COUNTY = 2;
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private SweetSunDB sweetsunDB;
+	private List<String> dataList = new ArrayList<String>();
+
+	private List<Province> provinceList;
+	private List<City> cityList;
+	private List<County> countyList;
+	
+	private Province selectedProvince;
+	private City selectedCity;
+	private County selectedCounty;
+	
+	private int currentLevel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(0x7f030000);
+		listView=(ListView)findViewById(0x7f060001);
+		titleText=(TextView)findViewById(0x7f060000);
+		adapter=new ArrayAdapter<String>(this,R.layout.simple_list_item_1,dataList);
+		listView.setAdapter(adapter);
+		sweetsunDB=SweetSunDB.getInstance(this);
+		listView.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				if(currentLevel==LEVEL_PROVINCE){
+					selectedProvince=provinceList.get(position);
+					queryCities();
+				}else if(currentLevel==LEVEL_CITY){
+					selectedCity=cityList.get(position);
+					queryCounties();
+				}
+				
+			}
+			
+		});
+		queryProvinces();
+	}
+	private void queryProvinces(){
+		provinceList=sweetsunDB.loadProvinces();
+		if(provinceList.size()>0){
+			dataList.clear();
+			for(Province province:provinceList){
+				dataList.add(province.getProvinceName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText("中国");
+			currentLevel=LEVEL_PROVINCE;
+			
+		}else {
+			queryFromServer(null,"province");
+		}
+	}
+	
+	
+	private void queryCities(){
+		cityList=sweetsunDB.loadCities(selectedProvince.getId());
+		if(cityList.size()>0){
+			dataList.clear();
+			for(City city:cityList){
+				dataList.add(city.getCityName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedProvince.getProvinceName());
+			currentLevel=LEVEL_CITY;
+			
+		}else {
+			queryFromServer(selectedProvince.getProvinceCode(),"city");
+		}
+	}
+	
+	private void queryCounties(){
+		countyList=sweetsunDB.loadCounties(selectedCity.getId());
+		if(countyList.size()>0){
+			dataList.clear();
+			for(County county:countyList){
+				dataList.add(county.getCountyName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedCity.getCityName());
+			currentLevel=LEVEL_COUNTY;
+			
+		}else {
+			queryFromServer(selectedCity.getCityCode(),"county");
+		}
+	}
+	
+	private void queryFromServer(final String code,final String type){
+		String address;
+		if(!TextUtils.isEmpty(code)){
+			address="http://www.weather.com.cn/data/list3/city"+code+".xml";
+		}else {
+			address="http://www.weather.com.cn/data/list3/city.xml";
+			
+		}
+		showProgressDialog();
+		HttpUtil.sendHttpRequest(address, new HttpCallbaceListener(){
+
+			@Override
+			public void onFinish(String response) {
+				// TODO Auto-generated method stub
+				boolean result=false;
+				if("province".equals(type)){
+					result=Utility.handleProvincesResponse(sweetsunDB, response);
+					
+				}else if("city".equals(type)){
+					result=Utility.handleCitesResponse(sweetsunDB, response, selectedProvince.getId());
+				}else if("county".equals(type)){
+					result=Utility.handleCountiesResponse(sweetsunDB, response, selectedCity.getId());
+				}
+				if(result){
+					runOnUiThread(new Runnable(){
+
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							closeProgressDialog();
+							if("province".equals(type)){
+								queryProvinces();
+							}else if("city".equals(type)){
+								queryCities();
+							}else if("county".equals(type)){
+								queryCounties();
+							}
+						}
+						
+					});
+				}
+			}
+
+			@Override
+			public void onError(Exception e) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable(){
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						closeProgressDialog();
+						Toast.makeText(ChooseAreaActivity.this, "加载失败", Toast.LENGTH_SHORT).show();
+						
+					}
+					
+				});
+				
+			}
+			
+		});
+	}
+	private void showProgressDialog(){
+		if(progressDialog==null){
+			progressDialog=new  ProgressDialog(this);
+			progressDialog.setMessage("正在加载....");
+			progressDialog.setCanceledOnTouchOutside(false);
+		}
+		progressDialog.show();
+	}
+	
+	private void closeProgressDialog(){
+		if(progressDialog!=null){
+			progressDialog.dismiss();
+		}
+	}
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		//super.onBackPressed();
+		if(currentLevel==LEVEL_COUNTY){
+			queryCities();
+		}else if(currentLevel==LEVEL_CITY){
+			queryProvinces();
+		}else{
+			finish();
+		}
+	}
+	
+}
--- src/com/sweetsun/app/util/Utility.java
+++ src/com/sweetsun/app/util/Utility.java
@@ -3,6 +3,8 @@ package com.sweetsun.app.util;
 import android.text.TextUtils;
 
 import com.sweetsun.app.SweetSunDB;
+import com.sweetsun.app.model.City;
+import com.sweetsun.app.model.County;
 import com.sweetsun.app.model.Province;
 public class Utility {
 
@@ -16,6 +18,46 @@ public class Utility {
 					province.setProvinceCode(array[0]);
 					province.setProvinceName(array[1]);
 					sweetsunDB.saveProvince(province);
+					
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	public static boolean handleCitesResponse(SweetSunDB sweetsunDB,String response, int provinceId){
+		if(!TextUtils.isEmpty(response)){
+			String[] allCites=response.split(",");
+			if(allCites!=null && allCites.length>0){
+				for(String p:allCites){
+					String[] array=p.split("\\|");
+					City city=new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					sweetsunDB.saveCity(city);
+					
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public  static boolean handleCountiesResponse(SweetSunDB sweetsunDB,String response,int cityId){
+		if(!TextUtils.isEmpty(response)){
+			String[] allCounties=response.split(",");
+			if(allCounties!=null && allCounties.length>0){
+				for(String p:allCounties){
+					String[] array=p.split("\\|");
+					County county=new County();
+					county.setCountyCode(array[0]);
+					county.setCountyName(array[1]);
+					county.setCityId(cityId);
+					sweetsunDB.saveCounty(county);
 					
 				}
 				return true;
